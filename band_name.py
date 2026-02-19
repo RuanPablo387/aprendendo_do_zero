b3950bc31f087a5fac2d5bde60e849bb510fe5a3
Ruan Pablo <ruanpablo387r@gmail.com>
2026-02-19 06:54:48 -0300

    praticando um pouco

---
+++ band_name.py
@@ -0,0 +1,4 @@
+print('Bem vindo ao gerador de nomes de bandas')
+Cidade=input('Qual o nome da ciade em que voce cresceu?')
+Pet=input('Qual o nome do seu pet?')
+print(f'O nome da sua banda pode ser {Cidade} {Pet}')
