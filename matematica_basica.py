44d124ed73e26365a0dd90944923ce1734245770
Ruan Pablo <ruanpablo387r@gmail.com>
2026-02-18 23:15:41 -0300

    Aprendendo problemas matematicos no Phyton.py

---
+++ matematica_basica.py
@@ -0,0 +1,11 @@
+num1 = int(input('Digite um numero'))
+num2 = int(input ('Digite outro numero'))
+num3 = int(input('Digite o ultimo numero'))
+resultado_adição = num1 + num2 + num3
+resultado_subtração = num1 - num2 - num3
+resultado_multiplicação = num1 * num2 * num3
+resultado_divisão = num1 /  num2 /  num3 
+print(resultado_adição)
+print(resultado_subtração)
+print(resultado_multiplicação)
+print(resultado_divisão)
