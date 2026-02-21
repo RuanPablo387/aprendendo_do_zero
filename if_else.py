bfd73a012bd33f359e011272f359283c3875b01e
Ruan Pablo <ruanpablo387r@gmail.com>
2026-02-21 03:59:16 -0300

    Praticando mais um pouco

--- if_else.py
+++ if_else.py
@@ -1,4 +1,4 @@
-#Um lutador precisa bater o peso para sua luta, em sua categoria o peso maximo é 62.6kgs
+# Um lutador precisa bater o peso para sua luta, em sua categoria o peso maximo é 62.6kgs
 peso_maximo = 62.6
 peso_lutador=float(input('Qual o seu peso?'))
 if peso_lutador > peso_maximo:
@@ -6,16 +6,24 @@ if peso_lutador > peso_maximo:
 else:
  print('Parabens, voce bateu o peso, boa sorte na sua luta.')
 
-
-
+# Mesma coisa mas agora ultilzando o elif
 peso_boxeador=float(input('Qual o seu preso?'))
-abaixo_do_peso=59.9
-acima_do_peso=62.7
-peso_ideal=62.6
-if peso_boxeador > abaixo_do_peso:
- print('Boa, bateu o peso')
-elif peso_boxeador < acima_do_peso:
+abaixo_do_peso=60
+acima_do_peso=62.6
+if peso_boxeador < abaixo_do_peso:
+ print('Voce esta abaixo do peso')
+elif peso_boxeador <= acima_do_peso:
  print('Boa, bateu o peso')
 else:
- print('infelizmente voce nao bateu o peso')
+ print('voce esta acima do peso')
 
+
+# Mais um pra praticar
+velocidade_maxima=70
+velocidade_carro=float(input('Qual velocidade seu carro estava quando passou pelo radar?'))
+if velocidade_carro >= 100:
+ print('Voce ultrapassou por muito o limite de velocidade permitido pela via, portanto vai receber uma multa GRAVE')
+elif velocidade_carro > velocidade_maxima:
+ print('Infelizmente seu carro ultrapassou o limite de velocidade nessa via e voce foi multado')
+else:
+ print('voce nao ultrapassou o limite permitido pela via, pode seguir viagem')
